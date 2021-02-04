5aa804cd74c00253c3606959678f99f5957ae81a
antonio <antoniomanzari@gmail.com>
2021-02-04 13:16:26 -0500

    trying my first commit again

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LibraryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Antonio was here!");
+            Console.WriteLine("This is a commit coming from Nick's branch");
+        }
+    }
+}
