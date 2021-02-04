dcf9a6b94143cb9e3d43efca520a6f542b74c945
17dorin <76854877+17dorin@users.noreply.github.com>
2021-02-04 13:46:37 -0500

    Help (#14)
    * Testing merging again
    
    * more headache for nick
    
    Co-authored-by: jrgallentine <76459959+jrgallentine@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ namespace LibraryProject
             Console.WriteLine("Hello World!");
             Console.WriteLine("idk if this is working");
             Console.WriteLine("This is a commit coming from Nick's branch");
+            Console.WriteLine("Another commit from nick");
+            Console.WriteLine("more fuel for the fire");
             Console.WriteLine("Antonio was here!");
+
         }
     }
 }
