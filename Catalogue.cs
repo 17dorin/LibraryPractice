8f4329cd38088765dcd166cbdf45a269c3b38ccc
Nick <17dorin@gmail.com>
2021-02-04 14:52:02 -0500

    Added catalogue class

---
+++ Catalogue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    class Catalogue
+    {
+        List<Book> Books { get; set; }
+
+        public Catalogue()
+        {
+            //Initialize Book objects and place them into a list here
+        }
+    }
+}
