3a1e15448a5c71103067104b045e3c12808a8912
17dorin <76854877+17dorin@users.noreply.github.com>
2021-02-04 14:47:18 -0500

    Nick (#17)
    * Hopefully Tommy isn't looking at the pull request history (#12)
    
    * antonios first commit for testing
    
    * Revert "antonios first commit for testing"
    
    * Revert "antonios first commit for testing"
    
    * hmmm (#8)
    
    Co-authored-by: Josh <joshua.r.gallentine@gmail.com>
    
    * Merge pull request #7 from 17dorin/main (#9)
    
    * Revert "Merge pull request #5 from 17dorin/main"
    
    Co-authored-by: antonio <antoniomanzari@gmail.com>
    Co-authored-by: Josh <joshua.r.gallentine@gmail.com>
    
    * Trying to decyper the arcane art of Git
    
    * Git kinda sucks
    
    * Added Book class
    
    Co-authored-by: antonio <antoniomanzari@gmail.com>
    Co-authored-by: Josh <joshua.r.gallentine@gmail.com>

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    public enum RentalStatus { CheckedOut, CheckedIn };
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public RentalStatus Status { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Book(string Title, string Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Status = RentalStatus.CheckedIn;
+        }
+
+
+    }
+}
