0e65fe5c9f53b70192ac1fdda10bfa6b43609915
antonio <antoniomanzari@gmail.com>
2021-02-04 11:37:41 -0500

    antonios first commit for testing

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace LibraryProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Antonio was here!");
         }
     }
 }
