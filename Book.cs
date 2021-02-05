7d9fa7e6b6b3529e46339a3602db317ea733cc0e
jrgallentine <76459959+jrgallentine@users.noreply.github.com>
2021-02-05 10:01:58 -0500

    Added display menu

---
+++ Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProject
+{
+    public enum RentalStatus { Out, In };
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public RentalStatus Status { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Book(string Title, string Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.Status = RentalStatus.In;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,18 +6,11 @@ namespace LibraryProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("idk if this is working");
-            Console.WriteLine("This is a commit coming from Nick's branch");
-            Console.WriteLine("Another commit from nick");
-            Console.WriteLine("This is sorcery");
-            Console.WriteLine("more fuel for the fire");
-            Console.WriteLine("Antonio was here!");
-        }
+            Catalog catalog = new Catalog();
 
-        static void FancyMethod()
-        {
-            // This was created by ANTONIO while checked out on branch: Antonio
+            catalog.DisplayMenu();
+          
+
         }
     }
 }
