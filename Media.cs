949aeeb23e825f85ed0964a372beb5b583a915e3
antonio <antoniomanzari@gmail.com>
2021-02-06 13:38:40 -0500

    moved methods from book to media base class

--- Media.cs
+++ Media.cs
@@ -7,13 +7,20 @@ namespace LibraryProject
     //Base class to derive other media from
     public enum RentalStatus { Out, In };
 
-    class Media
+    abstract class Media
     {
         public RentalStatus Status { get; set; }
         public DateTime DueDate { get; set; }
 
         public Media()
         {
+            Status = RentalStatus.In;
+        }
+        public abstract void CheckOut();
+
+        public void Return()
+        {
+            DueDate = DateTime.MaxValue;
             Status = RentalStatus.In;
         }
     }
